c4e1a63e02772c8c8c34ef86d819e54d3591fb2d
Kashima419 <109320096+Kashima419@users.noreply.github.com>
2022-08-25 15:03:20 +0900

    Update Get elements Category.py

--- Get elements Category.py
+++ Get elements Category.py
@@ -1,12 +1,21 @@
-
 from Autodesk.Revit.DB import Element
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 selection = [doc.GetElement(x) for x in uidoc.Selection.GetElementIds()]
 
-#get category
-lst = []
-for i in selection:
-	lst.append(i.Category.Name)
 
-print(lst)
+#get category
+for i in selection:
+	print(i.Category.Name)
+print('=================================================================')
+
+#get category list => error unicode
+category_name = []
+for i in selection:
+	category_name.append(i.Category.Name)
+print(category_name)
+
+print('=================================================================')
+#get category set
+for i in set(category_name):
+	print(i)
